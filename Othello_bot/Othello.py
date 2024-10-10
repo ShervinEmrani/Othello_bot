d456973c75c5556823f90ce246d6010decb34a1d
Shervin Emrani <118772042+ShervinEmrani@users.noreply.github.com>
2024-10-10 22:12:35 +0330

    Add files via upload

---
+++ Othello_bot/Othello.py
@@ -0,0 +1,237 @@
+from copy import deepcopy
+from print_board import print_matrix
+from ai import AI
+
+
+def in_board(_i, _j, r, c):
+    return (0 <= _i <= r - 1) and (0 <= _j <= c - 1)
+
+class Othello:
+
+    def __init__(self, useAI=False):
+        self.board = []
+        self.useAI = useAI
+
+    def legal_moves(self, board, _player):
+        empty_surrounding_cells = self.find_empty(board)
+        next_moves, pair_moves = self.find_correct_moves(board, empty_surrounding_cells, _player)
+        next_moves = list(set(next_moves))
+        next_moves.sort(key=lambda x: x[0])
+        return next_moves, empty_surrounding_cells, pair_moves
+
+    def end_game(self, board):
+        all_pieces = [i for elem in board for i in elem]
+        no_white_pieces = sum(1 for i in all_pieces if i == 'w')
+        no_black_pieces = sum(1 for i in all_pieces if i == 'b')
+        no_empty_cells = sum(1 for i in all_pieces if i == 'e')
+
+        # There is no empty cell to doing a valid move:
+        if no_empty_cells == 0 and no_white_pieces + no_black_pieces == len(all_pieces):
+            return True
+
+        # There is some empty cell but there is no any valid moves any more:
+        white_valid_moves = self.find_correct_moves(board, self.find_empty(board), 'w')[0]
+        black_valid_moves = self.find_correct_moves(board, self.find_empty(board), 'b')[0]
+        if white_valid_moves == [] and black_valid_moves == []:
+            return True
+
+        return False
+
+    def p2p_manager(self):
+        player = 'b'
+        arr = [['e' for i in range(8)] for j in range(8)]
+        arr[3][3] = 'w'
+        arr[4][4] = 'w'
+        arr[4][3] = 'b'
+        arr[3][4] = 'b'
+
+        while not self.end_game(arr):
+            if len(self.legal_moves(arr, player)[0]) == 0:
+                print(f"{'white' if player == 'w' else 'black'} has no legal move, so changed the turn.")
+                player = 'w' if player == 'b' else 'b'
+            print_matrix(arr, [], self.legal_moves(arr, player)[0])
+            try:
+                ind = tuple(map(int, input(f"it's {'white' if player == 'w' else 'black'} turn:\t").split(" ")))
+            except ValueError:
+                print(f"ValueError: INDEX OF CELLS MUST BE LIKE, X Y")
+                exit()
+
+            print('------------------------------')
+            while ind not in self.legal_moves(arr, player)[0]:
+                print("\tYou Entered Illegal Move!")
+                print("\tTry again, ", end='')
+                ind = tuple(map(int, input(f"it's {'white' if player == 'w' else 'black'} turn:\t").split(" ")))
+
+            pair_of_move = self.legal_moves(arr, player)[2]
+            arr = self.change_between(arr, ind, pair_of_move[ind], player)
+
+            # Check if the opponent has a valid move, then switch the turn.
+            opponent = 'b' if player == 'w' else 'w'
+            if self.find_correct_moves(arr, self.find_empty(arr), opponent)[0]:
+                player = 'b' if player == 'w' else 'w'
+
+            winner, draw = 0, False
+            if self.end_game(arr):
+                all_coins = [item for i in arr for item in i]
+                white_coins = sum(1 for i in all_coins if i == 'w')
+                black_coins = sum(1 for i in all_coins if i == 'b')
+                if black_coins != white_coins:
+                    winner = 'b' if black_coins > white_coins else 'w'
+                else:
+                    draw = True
+                print("---------END GAME---------")
+                if winner: print(
+                    f"Winner of the game is '{'Black player' if winner == 'b' else 'White player'}' and result "
+                    f"is: {max(white_coins, black_coins)} > {min(white_coins, black_coins)}")
+                if draw: print(
+                    f"Game is Draw and result is: {max(white_coins, black_coins)} = {min(white_coins, black_coins)}")
+
+    @staticmethod
+    def find_empty(board):
+        row = len(board)
+        col = len(board[0])
+        empties = []
+
+        for i in range(row):
+            for j in range(col):
+                if board[i][j] == 'e':
+                    continue
+                else:
+                    #              left_up, right_down, right_up, left_down, down, up, left, right
+                    directions = [(-1, -1), (1, 1), (-1, 1), (1, -1), (-1, 0), (1, 0), (0, -1), (0, 1)]
+                    for Dir in directions:
+                        _i, _j = Dir[0], Dir[1]
+                        if in_board(i + _i, j + _j, row, col):
+                            if board[i + _i][j + _j] == 'e':
+                                empties.append((i + _i, j + _j))
+
+        return list(set(empties))
+
+    @staticmethod
+    def find_correct_moves(board, empty_cells, player):
+        def add_key(dictionary, key, value):
+            try:
+                dictionary[key]
+            except KeyError:
+                temp = {}
+                temp[key] = value
+                dictionary.update(temp)
+                return dictionary
+            if dictionary != {}:
+                old_value = dictionary[key]
+                new_value = []
+                if type(old_value) == list:
+                    old_value: 'list'
+                    new_value = old_value.append(value)
+                else:
+                    old_value: 'tuple'
+                    new_value.append(old_value)
+                    new_value.append(value)
+                    dictionary[key] = new_value
+                return dictionary
+            else:
+                temp = {}
+                temp[key] = value
+                dictionary.update(temp)
+                return dictionary
+
+        #              left_up, right_down, right_up, left_down, down, up, left, right
+        directions = [(-1, -1), (1, 1), (-1, 1), (1, -1), (-1, 0), (1, 0), (0, -1), (0, 1)]
+        row, col = len(board), len(board[0])
+        correct_moves = []
+        pair_correct_moves = {}
+        opponent = 'b' if player == 'w' else 'w'
+
+        for index in empty_cells:
+            i = index[0]
+            j = index[1]
+
+            for Dir in directions:
+                _i = Dir[0]
+                _j = Dir[1]
+                if 0 <= i + _i <= row - 1 and 0 <= j + _j <= col - 1:
+                    if board[i + _i][j + _j] == opponent and in_board(i, j, row, col):
+                        ii, jj = i + 2 * _i, j + 2 * _j
+
+                        while in_board(ii, jj, row, col):
+                            if board[ii][jj] == opponent:
+                                ii += _i
+                                jj += _j
+                            elif board[ii][jj] != 'e':
+                                correct_moves.append((i, j))
+                                pair_correct_moves = add_key(pair_correct_moves, (i, j), (ii, jj))
+                                break
+                            else:
+                                break
+
+        return correct_moves, pair_correct_moves
+
+    @staticmethod
+    def change_between(board, move, pair_moves, turn):
+        arr = []
+        i = move[0]
+        j = move[1]
+        if type(pair_moves) == tuple:
+            pair_moves = [pair_moves]
+
+        for elem in pair_moves:
+            if i == elem[0]:
+                left_col = min(j, elem[1])
+                right_col = max(j, elem[1])
+                arr.extend([(i, j) for j in range(left_col + 1, right_col)])
+            if j == elem[1]:
+                down_row = max(i, elem[0])
+                up_row = min(i, elem[0])
+                arr.extend([(i, j) for i in range(up_row + 1, down_row)])
+            # diagonal
+            if abs(elem[0] - i) == abs(elem[1] - j):
+                ii = i
+                jj = j
+
+                # up-right
+                if elem[0] - i < 0 and elem[1] - j > 0:
+                    for k in range(elem[1] - j - 1):
+                        ii -= 1
+                        jj += 1
+                        arr.append((ii, jj))
+                # up-left
+                if elem[0] - i < 0 and elem[1] - j < 0:
+                    for k in range(abs(elem[1] - j) - 1):
+                        ii -= 1
+                        jj -= 1
+                        arr.append((ii, jj))
+                # down-right
+                if elem[0] - i > 0 and elem[1] - j > 0:
+                    for k in range(elem[1] - j - 1):
+                        ii += 1
+                        jj += 1
+                        arr.append((ii, jj))
+                # down-left
+                if elem[0] - i > 0 and elem[1] - j < 0:
+                    for k in range(elem[0] - i - 1):
+                        ii += 1
+                        jj -= 1
+                        arr.append((ii, jj))
+
+        board[move[0]][move[1]] = turn
+        for i in arr:
+            board[i[0]][i[1]] = turn
+        return board
+
+    def successor(self, board, turn):
+        result = self.legal_moves(board, turn)
+        next_move, empties, pair_moves = result[0], result[1], result[2]
+        successor_arr = []
+        if type(next_move) != list:
+            next_move = [next_move]
+        for move in next_move:
+            tmp = deepcopy(board)
+            tmp = self.change_between(tmp, move, pair_moves[move], turn)
+            successor_arr.append(tmp)
+        # for mat in successor_arr:
+        #     print_matrix(mat)
+        return successor_arr
+
+    def ai2ai_manager(self):
+        # after completing the AI class this game manager will be manage game between two ai player.
+        pass
---
+++ Othello_bot/minimax.py
@@ -0,0 +1,294 @@
+import copy
+import math
+from Othello import Othello
+from TranspositionTable import TranspositionTable
+from print_board import print_matrix
+import time
+game = Othello()
+
+
+def evaluate(board, player):
+    all_pieces = [i for elem in board for i in elem]
+    white_pieces = sum(1 for i in all_pieces if i == 'w')
+    black_pieces = sum(1 for i in all_pieces if i == 'b')
+    if white_pieces + black_pieces != len(all_pieces):
+        return numDiffe(board, player) + count_corners(board, player) + \
+               count_sides(board, player) + mobility(board, player) \
+               # + stability(board, player)
+    else:
+        return 0
+
+
+# ____________________________________________________________
+
+def numDiffe(board, turn):
+    all_pieces = [i for elem in board for i in elem]
+    white_pieces = sum(1 for i in all_pieces if i == 'w')
+    black_pieces = sum(1 for i in all_pieces if i == 'b')
+
+    if turn == 'b':
+        return (black_pieces / (black_pieces + white_pieces)) * 100
+    else:
+        return (white_pieces / (black_pieces + white_pieces)) * 100
+    # if white_pieces > black_pieces:
+    #     return (white_pieces / (black_pieces + white_pieces)) * 100
+    # else:
+    #     return - (black_pieces / (black_pieces + white_pieces)) * 100
+
+
+# ____________________________________________________________
+def count_corners(board, player):
+    # define the corners of the board
+    corners = [(0, 0), (0, 7), (7, 0), (7, 7)]
+    # initialize the counts for each player
+    white_count = 0
+    black_count = 0
+
+    # count the number of corners owned by each player
+    for i, j in corners:
+        if board[i][j] == "w":
+            white_count += 1
+        elif board[i][j] == "b":
+            black_count += 1
+
+    if player == 'b':
+        return 25 * black_count
+    elif player == 'w':
+        return 25 * white_count
+    else:
+        return float(0)
+    # return 25 * (white_count - black_count)
+
+
+# ____________________________________________________________
+def count_sides(board, player):
+    white_count = 0
+    black_count = 0
+
+    # count the number of side pieces owned by each player
+    for row in range(1, 7):
+        if board[row][0] == 'b':
+            black_count += 1
+        elif board[row][0] == 'w':
+            white_count += 1
+
+        if board[row][7] == 'b':
+            black_count += 1
+        elif board[row][7] == 'w':
+            white_count += 1
+
+    for col in range(1, 7):
+        if board[0][col] == 'b':
+            black_count += 1
+        elif board[0][col] == 'w':
+            white_count += 1
+
+        if board[7][col] == 'b':
+            black_count += 1
+        elif board[7][col] == 'w':
+            white_count += 1
+
+    if player == 'b':
+        return 4 * black_count
+    elif player == 'w':
+        return 4 * white_count
+    else:
+        return float(0)
+    # return 4 * (white_count - black_count)
+
+
+# ____________________________________________________________
+def mobility(board, player):
+    global game
+    black_mobility = len(game.successor(board, 'b'))
+    white_mobility = len(game.successor(board, 'w'))
+    # mobility = white_mobility / (white_mobility + black_mobility)
+    # calculate Possibility
+    # possibility = mobility * 100
+    if player == 'b' and (white_mobility + black_mobility) != 0:
+        return 100 * (black_mobility / (white_mobility + black_mobility))
+    elif player == 'w' and (white_mobility + black_mobility) != 0:
+        return 100 * (white_mobility / (white_mobility + black_mobility))
+    # return possibility
+
+
+# ____________________________________________________________
+# def stability(board, player):
+#     stability = [0, 0]
+#     blackStability = stability[0]
+#     whiteStability = stability[1]
+#
+#     opponent = 'b' if player == 'w' else 'w'
+#
+#     for i in range(8):
+#         for j in range(8):
+#             stable_pieces = 0
+#             if board[i][j] == 0:
+#                 continue
+#
+#             if (j == 0 or board[i][j - 1] == opponent) and (j == 7 or board[i][j + 1] == opponent):
+#                 stable_pieces += 1
+#
+#             if (i == 0 or board[i - 1][j] == opponent) and (i == 7 or board[i + 1][j] == opponent):
+#                 stable_pieces += 1
+#
+#             if (i == 0 or j == 0 or board[i - 1][j - 1] == opponent) and (
+#                     i == 7 or j == 7 or board[i + 1][j + 1] == opponent):
+#                 stable_pieces += 1
+#
+#             if (i == 0 or j == 7 or board[i - 1][j + 1] == opponent) and (
+#                     i == 7 or j == 0 or board[i + 1][j - 1] == opponent):
+#                 stable_pieces += 1
+#
+#             if stable_pieces >= 7:
+#                 stability[board[i][j] - 1] -= 1
+#
+#             elif stable_pieces <= 3:
+#                 stability[board[i][j] - 1] += 1
+#
+#     whiteStability = stability[1]
+#     blackStability = stability[0]
+#     if whiteStability + blackStability == 0:
+#         return 0
+#     else:
+#         stability = whiteStability / (whiteStability + blackStability)
+#         return 100 * stability
+
+
+# ____________________________________________________________
+def alpha_beta(board, depth, alpha, beta, maximizer, active_player, current_level=0, transposition_table=TranspositionTable()):
+    result = transposition_table.retrieve(board, depth)
+    if result:
+        return result
+
+    if depth == 0 or current_level == depth:
+        result = None, evaluate(board, active_player)
+        transposition_table.store(board, result, depth)
+        return result
+
+
+    global game
+    children = game.successor(board, active_player)
+    if not children:
+        op = 'b' if active_player == 'w' else 'w'
+        children = game.successor(board, op)
+    if maximizer and children:
+        # print(f"in maximizer children is: {'is empty' if not children else 'ful'}")
+        # print(f"depth is: {depth}")
+        best_move = children[0]
+
+    # since the first round is the given player, it starts by maximizing the utility
+    if maximizer:
+        max_list = []
+        max_eval = -math.inf
+        if type(children) != list:
+            children = [children]
+        for child in children:
+            board_copy = copy.deepcopy(child)
+            opponent0 = 'w' if active_player == 'b' else 'w'
+            current_eval = alpha_beta(board_copy, depth - 1, alpha, beta, False, opponent0, current_level + 1)[1]
+            tup = (child, current_eval)
+            max_list.append(tup)
+            current_eval = alpha_beta(board_copy, depth - 1, alpha, beta, False, active_player, current_level + 1,transposition_table)[1]
+            child_value_max = (child, current_eval)
+            max_list.append(child_value_max)
+
+            if current_eval > max_eval:
+                max_eval = current_eval
+            alpha = max(alpha, current_eval)
+            if beta <= alpha:
+                break
+
+        max_tuple = max(max_list, key=lambda p: p[1])
+        best_move = max_tuple[0]
+        max_value = max_tuple[1]
+        result = best_move, max_value
+
+        transposition_table.store(board, result, depth)
+        return result
+    # after the first round, since we are calculating the heuristic of opponent, we switch to minimizer
+
+    elif not maximizer and children:
+        # print(f"in minimizer children is: {children}")
+        min_list = []
+        for child in children:
+            # taking a copy of each child so we can change it
+            board_copy = copy.deepcopy(child)
+            opponent1 = 'b' if active_player == 'w' else 'b'
+            current_eval = alpha_beta(board_copy, depth - 1, alpha, beta, True, opponent1, current_level + 1)[1]
+            tup = (child, current_eval)
+            min_list.append(tup)
+            if beta <= alpha:
+                break
+            # calling minimax on each child (copy)
+            # current_eval is the value of heuristic
+            current_eval = alpha_beta(board_copy, depth - 1, alpha, beta, True, active_player, current_level + 1,transposition_table)[1]
+            # a tuple of each child with it's heristic value
+            child_value_min = (child, current_eval)
+            # adding the tuples of child & eval to the list
+            min_list.append(child_value_min)
+            # alpha, beta pruning for minimizer
+            if current_eval < min_eval:
+                min_eval = current_eval
+                best_move = child
+            beta = min(beta, current_eval)
+            if beta <= alpha:
+                break
+
+        # choosing the best child by it's heuristic value
+        min_tuple = min(min_list, key=lambda p: p[1])
+        best_move = min_tuple[0]
+        min_value = min_tuple[1]
+        result = best_move, min_value
+        transposition_table.store(board, result, depth)
+        return result
+    else:
+        return 0, 0
+
+
+
+if __name__ == "__main__":
+    """
+        AI TO AI MANAGER WITH ALPHA BETA PRUNING
+    """
+    player = 'b'
+    arr = [['e' for i in range(8)] for j in range(8)]
+    arr[3][3] = 'w'
+    arr[4][4] = 'w'
+    arr[4][3] = 'b'
+    arr[3][4] = 'b'
+
+    depth = 5
+    print_matrix(arr)
+    t = alpha_beta(arr, depth, -float('inf'), float('inf'), True, player, 0)
+    print_matrix(t[0])
+    print(t[1])
+    arr = t[0]
+
+    while not game.end_game(arr):
+        opponent = 'b' if player == 'w' else 'w'
+        if len(game.successor(arr, opponent)) != 0:
+            player = 'b' if player == 'w' else 'w'
+        print(f"-------------------{'white'if player=='w' else 'black'}-------------------")
+        print_matrix(t[0], [], game.legal_moves(arr, player)[0])
+        start = time.time()
+        t = alpha_beta(arr, depth, -float('inf'), float('inf'), True, player, 0)
+        print(time.time()-start)
+        arr = t[0]
+        winner, draw = 0, False
+        if game.end_game(arr):
+            all_coins = [item for i in arr for item in i]
+            white_coins = sum(1 for i in all_coins if i == 'w')
+            black_coins = sum(1 for i in all_coins if i == 'b')
+            if black_coins != white_coins:
+                winner = 'b' if black_coins > white_coins else 'w'
+            else:
+                draw = True
+            print(f"-------------------{'white'if player=='w' else 'black'}-------------------")
+            print_matrix(t[0], [], game.legal_moves(arr, player)[0])
+            print("---------END GAME---------")
+            if winner: print(
+                f"Winner of the game is '{'Black player' if winner == 'b' else 'White player'}' and result "
+                f"is: {max(white_coins, black_coins)} > {min(white_coins, black_coins)}")
+            if draw: print(
+                f"Game is Draw and result is: {max(white_coins, black_coins)} = {min(white_coins, black_coins)}")
